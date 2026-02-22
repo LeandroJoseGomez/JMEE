d467aabdfd4820ba4b1ae9e8de4649e0a3badebf
leandro <gomezleandrof6@gmail.com>
2026-02-22 19:58:08 -0400

    aviso de Copyright y cambio de version en archivo pom

--- src/main/java/parser/ExpressionHandler.java
+++ src/main/java/parser/ExpressionHandler.java
@@ -1,3 +1,19 @@
+/*
+    Copyright (C) 2026  Leandro J. Gómez
+
+    Este programa es software libre: usted puede redistribuirlo y/o modificarlo
+    bajo los términos de la Licencia Pública General GNU publicada por
+    la Fundación para el Software Libre, ya sea la versión 3 de la Licencia, o
+    (a su elección) cualquier versión posterior.
+
+    Este programa se distribuye con la esperanza de que sea útil,
+    pero SIN NINGUNA GARANTÍA; sin incluso la garantía implícita de
+    MERCANTILIDAD o APTITUD PARA UN PROPÓSITO PARTICULAR. Consulte la
+    Licencia Pública General GNU para más detalles.
+
+    Usted debería haber recibido una copia de la Licencia Pública General GNU
+    junto con este programa. Si no es así, consulte <https://www.gnu.org/licenses/>.
+ */
 
 package parser;
 
