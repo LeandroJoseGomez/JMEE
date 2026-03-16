ec166d2a8af81a6f16bd576cf1174e2a8bd90f26
leandro <gomezleandrof6@gmail.com>
2026-03-15 22:35:49 -0400

    Test: OperatorRegistryTest

--- src/test/java/operators/OperatorRegistryTest.java
+++ src/test/java/operators/OperatorRegistryTest.java
@@ -1,16 +1,65 @@
 package operators;
 
+import functions.Function;
+import functions.FunctionRegistry;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OperatorRegistryTest {
 
+    private final String[] OP_SYMBOLS = {"+", "-", "*", "/", "^"};
+
+
     @Test
     void get() {
+        for(String operatorSymbol : OP_SYMBOLS){
+            Operator op = OperatorRegistry.get(operatorSymbol);
+            assertNotNull(op, "Operador no encontrado");
+        }
     }
 
     @Test
     void isOperator() {
+        for(String operatorSymbol : OP_SYMBOLS){
+            assertTrue(OperatorRegistry.isOperator(operatorSymbol));
+        }
+    }
+
+    @Test
+    void register() {
+         Operator operator = new Operator() {
+            @Override
+            public String getSymbol() {
+                return "%";
+            }
+
+            @Override
+            public int getPrecedence() {
+                return 2;
+            }
+
+            @Override
+            public int getArgsCount() {
+                return 2;
+            }
+
+            @Override
+            public boolean isLeftAssociative() {
+                return true;
+            }
+
+            @Override
+            public BigDecimal execute(MathContext mathContext, BigDecimal... args) {
+                return (args[0].divide(args[1], mathContext)).pow(2);
+            }
+        };
+
+        OperatorRegistry.register(operator);
+
+        assertTrue(OperatorRegistry.isOperator(operator.getSymbol()));
     }
 }
