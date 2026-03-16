17b6eccfe4341e910cc1d952172f2325d6324feb
leandro <gomezleandrof6@gmail.com>
2026-03-15 21:31:18 -0400

    Test: EvaluatorTest

--- src/test/java/evaluator/EvaluatorTest.java
+++ src/test/java/evaluator/EvaluatorTest.java
@@ -1,7 +1,6 @@
 package evaluator;
 
 import org.junit.jupiter.api.Test;
-import parser.Parser;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
@@ -10,56 +9,54 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EvaluatorTest {
+    private List<String> psofixExpression;
+    BigDecimal expResult;
 
     @Test
     void evaluateExpression() {
 
-        List<String> psofixExpression = Arrays.asList
+        psofixExpression = Arrays.asList
                                         ("-7", "-3", "-3.33", "4", "*", "+", "-20", "10", "/", "-", "2", "^", "/");
-        double expResult = -0.034135950813020816;
+        expResult = new BigDecimal("-0.03413595081302081707811866046627758");
 
-        BigDecimal result = Evaluator.evaluateExpression(psofixExpression);
-        assertEquals(expResult, result);
+        assertEquals(expResult, Evaluator.evaluateExpression(psofixExpression));
     }
 
     @Test
     void evaluateExpression_2() {
-        List<String> psofixExpression = Arrays.asList
-                ("0", "sin", "-5.5", "-2", "abs", "*", "-");
-        double expResult = 11;
+        psofixExpression = Arrays.asList("0", "sin", "-5.5", "-2", "abs", "*", "-");
+        expResult = new BigDecimal("11");
 
         BigDecimal result = Evaluator.evaluateExpression(psofixExpression);
-        assertEquals(expResult, result);
+        assertTrue(expResult.compareTo(result) == 0);
     }
 
     @Test
     void evaluateExpression_3() {
-        List<String> psofixExpression = Arrays.asList
-                ("-10.5", "2", "2", "^", "-3", "1", "+", "-", "/", "2", "*");
-        double expResult = -3.5;
+        psofixExpression = Arrays.asList("-10.5", "2", "2", "^", "-3", "1", "+", "-", "/", "2", "*");
+        expResult = new BigDecimal("-3.5");
 
         BigDecimal result = Evaluator.evaluateExpression(psofixExpression);
-        assertEquals(expResult, result);
+        assertTrue(expResult.compareTo(result) == 0);
     }
 
     @Test
     void evaluateExpression_4() {
-        List<String> psofixExpression = Arrays.asList
-                ("100", "log10", "3.5", "-1.5", "+", "2", "^", "*");
-        double expResult = 400;
+        psofixExpression = Arrays.asList("100", "log10", "3.5", "-1.5", "+", "2", "^", "*");
+        expResult = new BigDecimal("400");
 
         BigDecimal result = Evaluator.evaluateExpression(psofixExpression);
-        assertEquals(expResult, result);
+        assertTrue(expResult.compareTo(result) == 0);
     }
 
     @Test
     void evaluateExpression_5() {
         List<String> psofixExpression = Arrays.asList
-                ("2", "3", "2", "^", "^", "NEG", "500", "12", "+", "/", "1.5", "+");
-        double expResult = 2.5;
+                ("2", "3", "2", "^", "^", "500", "12", "+", "/", "1.5", "+");
+        expResult = new BigDecimal("2.5");
 
         BigDecimal result = Evaluator.evaluateExpression(psofixExpression);
-        assertEquals(expResult, result);
+        assertTrue(expResult.compareTo(result) == 0);
     }
 
 }
