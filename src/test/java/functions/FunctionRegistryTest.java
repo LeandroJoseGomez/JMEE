02368272159d4f336b9acbaad6c8265095233ac0
leandro <gomezleandrof6@gmail.com>
2026-03-15 21:57:33 -0400

    Test: FunctionRegistryTest

--- src/test/java/functions/FunctionRegistryTest.java
+++ src/test/java/functions/FunctionRegistryTest.java
@@ -2,19 +2,56 @@ package functions;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FunctionRegistryTest {
 
+    private final String[] FUN_NAMES = {"Sin", "cOs", "taN", "Log", "lN", "sqrt"};
+
     @Test
     void get() {
+        for(String functionName : FUN_NAMES){
+            Function fun = FunctionRegistry.get(functionName);
+            assertNotNull(fun, "Función no encontrada");
+        }
+
     }
 
     @Test
     void isFunction() {
+        for(String functionName : FUN_NAMES){
+            assertTrue(FunctionRegistry.isFunction(functionName));
+        }
     }
 
     @Test
     void register() {
+        Function function = new Function() {
+            @Override
+            public String getName() {
+                return "sum";
+            }
+
+            @Override
+            public int getArgsCount() {
+                return 2;
+            }
+
+            @Override
+            public int getPrecedence() {
+                return 3;
+            }
+
+            @Override
+            public BigDecimal execute(MathContext mathContext, BigDecimal... args) {
+                return args[0].add(args[1], mathContext);
+            }
+        };
+        FunctionRegistry.register(function);
+
+        assertTrue(FunctionRegistry.isFunction(function.getName()));
     }
 }
